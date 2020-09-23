2ef31c545ab456d37619f967e3050662c723643c
Felipe França <contatofelipefranca12@gmail.com>
2020-09-23 19:14:11 -0300

    Resolução do exercício IMC

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Índice de massa corporal");
+
+            Console.Write("Altura (m)..: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Peso (kg)...:");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+            Console.WriteLine($"IMC: {imc} kg/m²");
+        }
+    }
+}
